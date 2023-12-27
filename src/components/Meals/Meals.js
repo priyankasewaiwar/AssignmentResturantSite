fbb1eff92a6dcf595984287a225d2b7bd9a1e10b
priyankasewaiwar <priynkadohare@gmail.com>
2023-12-27 23:14:11 +0530

    Meal Item Add FOrm

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,12 +1,15 @@
-import { Fragment } from "react";
-import MealsSummary from "./MealsSummary";
-import AvailableMeals from "./AvailableMeals";
+import { Fragment } from 'react';
 
-const Meals = () =>{
-return <Fragment >
-    <MealsSummary />
-    <AvailableMeals />
-</Fragment>
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
 };
 
-export default Meals
+export default Meals;
